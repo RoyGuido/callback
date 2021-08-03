593049b3de7b082059a65f3e687ee7a0c99a5aa8
Roy Salazar Herrera <roy.salazar@bg.com.bo>
2021-08-03 11:44:59 -0400

    Initial commit

---
+++ demopost-master/index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const routes = express.Router();
+const app = express();
+const fs = require('fs');
+const { encrypt, decrypt } = require('./services/jweServices');
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+routes.get('/', (req, res)=>{  
+    res.json({message:'ok'});
+});
+
+routes.post('/', (req, res)=>{  
+    const { body } = req;
+    //console.log((body));
+    res.send('ok');
+});
+routes.post('/v1', (req, res)=>{  
+    const { body } = req;
+    //console.log('v1-----------------------------------------');
+    //console.log((body));
+    res.send('ok');
+});
+
+routes.post('/v2', (req, res)=>{  
+    const { body } = req;
+    //console.log('v2-----------------------------------------');
+    //console.log((body));
+    res.send('ok');
+});
+
+routes.post('/demo',async (req, res)=>{
+    const { body } = req;
+    console.log('Body-----------------------------------------');
+    console.log(body);
+    const pemPrivate = fs.readFileSync('./assets/pemPrivate.txt', 'utf8');
+    const result = await decrypt(pemPrivate, body.msgCallback);
+    console.log('Result-----------------------------------------');
+    console.log(result)
+    res.send('ok');
+})
+
+app.use(routes);
+
+const port = process.env.PORT || 3050;
+
+app.listen(port, ()=>{
+    console.log(`Corriendo en el puerto:${port}` )
+});
